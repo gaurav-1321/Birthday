324ec1f29ac2b7d2b74748d307cb3241ea3af071
Gaurav Singh Panwar <134685568+gaurav-1321@users.noreply.github.com>
2026-07-23 11:28:39 +0530

    Rename FromRexon container to FromGaurav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -98,7 +98,7 @@ const Home = () => {
                                     Click Here Ashna
                                 </div>
                             </button>
-                            <div id="FromRexon">
+                            <div id="FromGaurav">
                                 <Link to={'/letter'} className="flex items-center gap-2 md:text-[1rem] text-sm">
                                     <svg className='w-7 h-7' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                         <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
